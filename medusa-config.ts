532638a9cee6f594c6529d64bc839bf5e383acf3
jaykanjia <jaykanjia11@gmail.com>
2025-05-14 10:30:07 +0530

    initial commit

---
+++ medusa-config.ts
@@ -0,0 +1,68 @@
+import { loadEnv, defineConfig } from "@medusajs/framework/utils";
+
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
+
+module.exports = defineConfig({
+	projectConfig: {
+		databaseUrl: process.env.DATABASE_URL,
+		http: {
+			storeCors: process.env.STORE_CORS!,
+			adminCors: process.env.ADMIN_CORS!,
+			authCors: process.env.AUTH_CORS!,
+			jwtSecret: process.env.JWT_SECRET || "supersecret",
+			cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+		},
+	},
+	plugins: [
+		{
+			resolve: "@rokmohar/medusa-plugin-meilisearch",
+			options: {
+				config: {
+					host: process.env.MEILISEARCH_HOST ?? "",
+					apiKey: process.env.MEILISEARCH_API_KEY ?? "",
+				},
+				settings: {
+					// The key is used as the index name in Meilisearch
+					products: {
+						// Required: Index type
+						type: "products",
+						// Optional: Whether the index is enabled. When disabled:
+						// - Index won't be created or updated
+						// - Documents won't be added or removed
+						// - Index won't be included in searches
+						// - All operations will be silently skipped
+						enabled: true,
+						// Optional: Specify which fields to include in the index
+						// If not specified, all fields will be included
+						fields: [
+							"id",
+							"title",
+							"description",
+							"handle",
+							"variant_sku",
+							"thumbnail",
+						],
+						indexSettings: {
+							searchableAttributes: ["title", "description", "variant_sku"],
+							displayedAttributes: [
+								"id",
+								"handle",
+								"title",
+								"description",
+								"variant_sku",
+								"thumbnail",
+							],
+							filterableAttributes: ["id", "handle"],
+						},
+						primaryKey: "id",
+						// Create your own transformer
+						/*transformer: (product) => ({
+              id: product.id,
+              // other attributes...
+            }),*/
+					},
+				},
+			},
+		},
+	],
+});
